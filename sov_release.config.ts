2a6476f1c789e5c145207e1ac8ca9ea0025114b5
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-04-30 11:04:53 +0200

    Release: 2.0.1

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -5,7 +5,7 @@ const releaseConfig: ReleaseConfig = {
     {
       directory: "./",
       release: {
-        version: "2.0.0",
+        version: "2.0.1",
         versionBumper: [
           {
             filePath: "ps_sovendus/constants.php",
